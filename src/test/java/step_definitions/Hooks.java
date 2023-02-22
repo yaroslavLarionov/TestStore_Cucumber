6229ff5e75792fbf4ff96d231b4f0744f5308cd7
yaroslavLarionov <yarillo@yahoo.com>
2023-02-21 22:06:57 -0500

    added Cucumber log utility class and updated Hooks class to reflect the changes

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -2,18 +2,26 @@ package step_definitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import utils.ConfigReader;
+import utils.CucumberLogUtils;
 import utils.WebDriverFactory;
 
 public class Hooks {
 
     @Before
-    public void setUp() {
+    public void setUp(Scenario scenario) {
         WebDriverFactory.getDriver().get(ConfigReader.getProperty("url"));
+        CucumberLogUtils.initScenario(scenario);
     }
 
     @After
-    public void tearDown() {
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            CucumberLogUtils.logFail(scenario.getName(), true);
+        } else {
+            CucumberLogUtils.logPass(scenario.getName(), true);
+        }
         WebDriverFactory.quitDriver();
     }
 }
